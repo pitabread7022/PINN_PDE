7837c1e909dabcc0ddaf483f9731b45d6c4fe7ee
Mubashshir Uddin <54147943+meandme234@users.noreply.github.com>
2021-06-11 09:44:11 +0530

    insert pip installing command in initial script

--- initialise.sh
+++ initialise.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+sudo apt-get update
+sudo apt install python3-pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py
+#python3 get-pip.py
 pip install virtualenv
 virtualenv -p /usr/bin/python3.6 PINN_env
 source PINN_env/bin/activate
@@ -11,3 +13,4 @@ pip install tensorflow==1.15.0
 apt-get install python3-tk
 pip install pyDOE
 pip install latex
+pip install -r requirements.txt
